55c1a27831d2e243638033a5f22ea5ad02af4754
khanh.ho <honhatnamkhanh10b1@gmail.com>
2025-11-08 13:36:10 +0700

    Update example_test.py

--- tests/Example_test.py
+++ tests/Example_test.py
@@ -1,14 +1,14 @@
 from playwright.sync_api import Page  # chỉ để hỗ trợ autocomplete (type hint)
 
-def test_google_homepage(page: Page):
+def test_google_homepage(page):
     page.goto("https://www.google.com")
     assert "Google" in page.title()
 
-def test_facebook_homepage(page: Page):
+def test_facebook_homepage(page):
     page.goto("https://www.facebook.com")
     assert "Facebook" in page.title()
 
-def test_google_search(page: Page):
+def test_google_search(page):
     page.goto("https://www.google.com")
     page.fill("textarea[name='q']", "Playwright Python")
     page.keyboard.press("Enter")
