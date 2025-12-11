f15153f087113ed45f0d4f66e9b4b938ae0d4391
khanh.ho <honhatnamkhanh10b1@gmail.com>
2025-12-11 20:15:01 +0700

    Fix login_test

--- tests/login_test.py
+++ tests/login_test.py
@@ -1,7 +1,7 @@
 from playwright.sync_api import Page 
 from pages.login_page import LoginPage
 
-def test_google_homepage(page: Page):
+def test_login_page(page: Page):
     page.goto("https://practice.automationtesting.in/my-account/")
     
     login = LoginPage(page)
@@ -10,4 +10,5 @@ def test_google_homepage(page: Page):
     # login.enter_password("password")
     # login.click_login()
     
-    login.login("kaceho@game.id", "password")
+    login.login("kaceho@id.game", "kaceho@id.game1")
+    page.wait_for_timeout(1000)
--- utils/report_utils.py
+++ utils/report_utils.py
@@ -2,13 +2,12 @@ import os
 from datetime import datetime
 
 def create_html_report_path(base_folder="reports"):
-    """Tạo đường dẫn report theo ngày + giờ"""
-    # 📅 Tạo thư mục theo ngày
+    # Create folder base day
     today = datetime.now().strftime("%Y-%m-%d")
     day_folder = os.path.join(base_folder, today)
     os.makedirs(day_folder, exist_ok=True)
 
-    # 🕒 Tạo timestamp cho file (giờ-phút-giây)
+    # create timestamp file (hour-min-sec)
     timestamp = datetime.now().strftime("%H-%M-%S")
     new_report_path = os.path.join(day_folder, f"report_{timestamp}.html")
 
